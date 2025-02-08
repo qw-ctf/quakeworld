2c07e8705797371f4df13684cbda7a5119c22e18
Jürgen Legler <jogihoogi@gmail.com>
2025-02-08 07:00:15 +0100

    add perf

---
+++ src/utils/perf.rs
@@ -0,0 +1,32 @@
+use core::time;
+use std::{fmt::Display, time::Instant};
+
+pub struct Perf {
+    start: Instant,
+    stop: Option<Instant>,
+}
+
+impl Perf {
+    pub fn start() -> Self {
+        Perf {
+            start: Instant::now(),
+            stop: None,
+        }
+    }
+    pub fn stop(&mut self) {
+        self.stop = Some(Instant::now());
+    }
+}
+
+impl Display for Perf {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.stop {
+            Some(stop) => {
+                write!(f, "{}", format!("{:?}", stop - self.start))
+            }
+            None => {
+                write!(f, "started: {:?}", self.start)
+            }
+        }
+    }
+}
