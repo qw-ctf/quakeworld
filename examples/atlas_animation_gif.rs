3a501b0a4860cc92aec1271e886319cee6b0fa9b
Jürgen Legler <jogihoogi@gmail.com>
2025-02-22 15:07:52 +0100

    add option to render all maps

--- examples/atlas_animation_gif.rs
+++ examples/atlas_animation_gif.rs
@@ -35,87 +35,110 @@ fn create_atlas(paks: Vec<String>, bspname: String) -> Result<bool, Box<dyn Erro
         vfs.insert_node(node, "/");
     }
 
-    let map_name = bspname.clone();
-    let bspname = format!("maps/{}.bsp", bspname.clone());
-
-    let bsp_data = vfs.read(bspname.clone(), None)?;
+    let mut maps: Vec<(String, Vec<u8>)> = vec![];
+    if bspname == "ALLMAPS" {
+        let map_names: Vec<String> = vec![];
+        let entries = vfs.list("/maps")?;
+        for entry in &entries {
+            for e in &entry.entries {
+                match e {
+                    quakeworld::vfs::VfsEntry::File(vfs_entry_file) => {
+                        let name = vfs_entry_file.path.last();
+                        let name_without_extension = name[0..name.len() - 4].to_string();
+                        let name = format!("maps/{}.bsp", name_without_extension);
+                        let bsp_data = vfs.read(name, None)?;
+                        maps.push((name_without_extension.clone(), bsp_data));
+                    }
+                    quakeworld::vfs::VfsEntry::Directory(vfs_entry_directory) => {}
+                }
+            }
+        }
+    } else {
+        let name = format!("maps/{}.bsp", bspname.clone());
+        let bsp_data = vfs.read(name, None)?;
+        maps.push((bspname.clone(), bsp_data));
+    }
     let palette_data = vfs.read("gfx/palette.lmp", None)?;
     // let palette = quakeworld::lmp::Palette::from(palette_data)?;
 
-    #[cfg(feature = "trace")]
-    let mut tr = Trace::new();
-    let b = Bsp::parse(
-        bsp_data.clone(),
+    for (bsp_name, bsp_data) in maps {
+        println!("creating {}_atlas.gif", bsp_name);
         #[cfg(feature = "trace")]
-        Some(&mut tr),
-    )?;
+        let mut tr = Trace::new();
+        let b = Bsp::parse(
+            bsp_data.clone(),
+            #[cfg(feature = "trace")]
+            Some(&mut tr),
+        )?;
 
-    let textures_list = b.textures.clone();
-    let statistics = quakeworld::texture::atlas::Statistics::gather(&textures_list, 0);
-    let mut atlas = quakeworld::texture::atlas::Atlas::new(512, 512);
-    atlas.insert_textures(statistics.textures);
-    let tiles_absolut = atlas.tiles.len();
-
-    let limit = b.textures.len();
-    let mut count_up = vec![];
-    for i in 1..limit {
-        count_up.push(i);
-    }
-
-    let mut frames: Vec<Vec<_>> = vec![];
-    let mut frame_width = 0;
-    let mut frame_height = 0;
-    for count in count_up {
-        let mut textures_list: Vec<TextureParsed> = vec![];
-        // textures for statistics
-        for n in 0..b.textures.len() as usize {
-            textures_list.push(b.textures[n].clone());
-        }
-
+        let textures_list = b.textures.clone();
         let statistics = quakeworld::texture::atlas::Statistics::gather(&textures_list, 0);
         let mut atlas = quakeworld::texture::atlas::Atlas::new(512, 512);
-        atlas.debug_boxes = true;
-        let mut new_texture_list = vec![];
-        for (c, t) in statistics.textures.iter().enumerate() {
-            if c == count {
-                break;
+        atlas.insert_textures(statistics.textures);
+        let tiles_absolut = atlas.tiles.len();
+
+        let limit = b.textures.len();
+        let mut count_up = vec![];
+        for i in 1..limit {
+            count_up.push(i);
+        }
+
+        let mut frames: Vec<Vec<_>> = vec![];
+        let mut frame_width = 0;
+        let mut frame_height = 0;
+        for count in count_up {
+            let mut textures_list: Vec<TextureParsed> = vec![];
+            // textures for statistics
+            for n in 0..b.textures.len() as usize {
+                textures_list.push(b.textures[n].clone());
             }
-            new_texture_list.push(t.clone());
-        }
-        atlas.insert_textures(new_texture_list);
-        let mut area = 0;
-        for (e, tile) in atlas.tiles.iter().enumerate() {
-            for t in &tile.textures {}
 
-            for b in &tile.boxes {
-                area += b.size.area();
+            let statistics = quakeworld::texture::atlas::Statistics::gather(&textures_list, 0);
+            let mut atlas = quakeworld::texture::atlas::Atlas::new(512, 512);
+            atlas.debug_boxes = true;
+            let mut new_texture_list = vec![];
+            for (c, t) in statistics.textures.iter().enumerate() {
+                if c == count {
+                    break;
+                }
+                new_texture_list.push(t.clone());
             }
-        }
-        if atlas.tiles.len() < tiles_absolut {
-            atlas.tiles.push(AtlasTile {
-                size: atlas.tile_size.clone(),
-                boxes: vec![],
-                textures: vec![],
-                full: false,
-            });
-        }
-        let atlas_map = atlas.generate_texture(&b.textures)?;
+            atlas.insert_textures(new_texture_list);
+            let mut area = 0;
+            for (e, tile) in atlas.tiles.iter().enumerate() {
+                for t in &tile.textures {}
 
-        frame_height = atlas_map.size.height as u16;
-        frame_width = atlas_map.size.width as u16;
-        frames.push(atlas_map.data);
-    }
+                for b in &tile.boxes {
+                    area += b.size.area();
+                }
+            }
+            if atlas.tiles.len() < tiles_absolut {
+                atlas.tiles.push(AtlasTile {
+                    size: atlas.tile_size.clone(),
+                    boxes: vec![],
+                    textures: vec![],
+                    full: false,
+                });
+            }
+            let atlas_map = atlas.generate_texture(&b.textures)?;
 
-    let mut image = File::create(format!("{}_atlas.gif", map_name)).unwrap();
-    let mut encoder = Encoder::new(&mut image, frame_width, frame_height, &palette_data).unwrap();
-    encoder.set_repeat(Repeat::Finite(1)).unwrap();
-    for frame_data in frames {
-        let mut frame = Frame::default();
-        frame.width = frame_width;
-        frame.height = frame_height;
-        frame.delay = 25;
-        frame.buffer = Cow::Borrowed(&*frame_data);
-        encoder.write_frame(&frame).unwrap();
+            frame_height = atlas_map.size.height as u16;
+            frame_width = atlas_map.size.width as u16;
+            frames.push(atlas_map.data);
+        }
+
+        let mut image = File::create(format!("{}_atlas.gif", bsp_name)).unwrap();
+        let mut encoder =
+            Encoder::new(&mut image, frame_width, frame_height, &palette_data).unwrap();
+        encoder.set_repeat(Repeat::Finite(1)).unwrap();
+        for frame_data in frames {
+            let mut frame = Frame::default();
+            frame.width = frame_width;
+            frame.height = frame_height;
+            frame.delay = 25;
+            frame.buffer = Cow::Borrowed(&*frame_data);
+            encoder.write_frame(&frame).unwrap();
+        }
     }
 
     Ok(true)
